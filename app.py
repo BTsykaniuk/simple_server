3b8f61dda65cc940ccb8c52bd86ef3447779cc7a
BTsykaniuk <btsykanjuk@i.ua>
2018-12-06 01:01:09 +0200

    Update project logic

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
-from flask import Flask
-from flask_mongoengine import MongoEngine
-import models
+from flask import Flask, request, render_template
+import requests
 
 
 app = Flask(__name__)
-app.config["MONGODB_SETTINGS"] = {'DB': "projectdb"}
-
-db = MongoEngine(app)
 
 
-def register_blueprints():
-    # Prevents circular imports
-    from view import users
-    app.register_blueprint(users)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
 
 
-register_blueprints()
+@app.route('/send/', methods=['POST'])
+def send_result():
+    data = request.form.to_dict()
+
+    if data['agree'] == 'Agree':
+        agree = True
+    else:
+        agree = False
+    mac = request.remote_addr
+    out = {'mac_adress': mac,
+           'agree': agree}
+    requests.post('http://127.0.0.1:5002/api/', json=out)
+    return 'Data send!'
 
 
 if __name__ == '__main__':
